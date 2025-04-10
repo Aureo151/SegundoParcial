b490b8311101a6f4d93c3ce130eaf3c72201d2ac
Aureo151 <aureotzic@gmail.com>
2025-04-10 08:42:32 -0600

    clase libros activa y funcionando

--- SegundoParcial/FormLibro.Designer.cs
+++ SegundoParcial/FormLibro.Designer.cs
@@ -32,6 +32,12 @@
             this.txtCodigo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -66,17 +72,69 @@
             this.txtNombre.Size = new System.Drawing.Size(180, 22);
             this.txtNombre.TabIndex = 3;
             // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(191, 144);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(180, 22);
+            this.txtAutor.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Autor del Libro";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(77, 198);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(114, 52);
+            this.btnGuardar.TabIndex = 6;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(421, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(342, 205);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(287, 198);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(109, 52);
+            this.btnMostrar.TabIndex = 8;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
             // FormLibro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 276);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtCodigo);
             this.Controls.Add(this.label1);
             this.Name = "FormLibro";
             this.Text = "FormLibro";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,5 +146,10 @@
         private System.Windows.Forms.TextBox txtCodigo;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnMostrar;
     }
 }
--- SegundoParcial/FormLibro.cs
+++ SegundoParcial/FormLibro.cs
@@ -12,9 +12,42 @@ namespace SegundoParcial
 {
     public partial class FormLibro : Form
     {
+        List<Libro> libros = new List<Libro>();
         public FormLibro()
         {
             InitializeComponent();
+        }
+
+        private void mostrar()
+        {
+            LibroArchivo libroArchivo = new LibroArchivo();
+            libros = libroArchivo.Leer("../../Libros.json");
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = libros;
+            dataGridView1.Refresh();
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Libro libro = new Libro();
+
+            libro.codigo = txtCodigo.Text;
+            libro.nombre_libro = txtNombre.Text;
+            libro.autor = txtAutor.Text;
+
+            libros.Add(libro);
+
+            LibroArchivo libroArchivo = new LibroArchivo();
+            libroArchivo.Guardar("../../Libros.json", libros);
+
+            txtNombre.Clear();
+            txtAutor.Clear();
+            txtCodigo.Clear();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            mostrar();
         }
     }
 }
